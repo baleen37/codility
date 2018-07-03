512a7c7e1972bbd551aec93f8123c56fc385db2e
Jiho Lee <baleen37@gmail.com>
2018-07-03 14:52:44 +0900

    Add large case in PermCheck

--- test/com.codility/PermCheckTest.java
+++ test/com.codility/PermCheckTest.java
@@ -21,4 +21,25 @@ class PermCheckTest {
         assertEquals(0, pc.solution(new int[]{2}));
         assertEquals(1, pc.solution(new int[]{}));
     }
+
+    @Test
+    void mediumCase(){
+        PermCheck pc = new PermCheck();
+        assertEquals(1, pc.solution(new int[]{10,2,3,6,4,5,7,8,9,1}));
+        assertEquals(0, pc.solution(new int[]{10,2,3,6,4,5,6,7,7,9}));
+    }
+
+    @Test
+    void largeCase(){
+        PermCheck pc = new PermCheck();
+        int[] intArray = new int[100000];
+        for(int i = 0; i < intArray.length; i++){
+            intArray[i] = i+1;
+        }
+
+        assertEquals(1, pc.solution(intArray));
+
+        intArray[2] =1;
+        assertEquals(0, pc.solution(intArray));
+    }
 }
