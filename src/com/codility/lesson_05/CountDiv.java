313d77530df669e804681c0847e9b8fbbf40b1e5
Jiho Lee <baleen37@gmail.com>
2018-07-04 15:12:15 +0900

    Add CountDiv

--- src/com/codility/lesson_05/CountDiv.java
+++ src/com/codility/lesson_05/CountDiv.java
@@ -2,7 +2,7 @@ package com.codility.lesson_05;
 
 public class CountDiv {
     public int solution(int A, int B, int K) {
-        int b = B / K;
+       int b = B / K;
         int a = (A > 0 ? (A - 1) / K : 0);
         if (A == 0) {
             b++;
